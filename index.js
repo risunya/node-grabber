f880a6c2b1b8898b53f3eae68d1a71d328989e6a
risunya <risunyaaa@yandex.ru>
2025-01-25 12:37:07 +0300

    feat: add requests for checking wich one group is sending messages

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
-require('dotenv').config();
+const { Bot } = require("grammy");
 const { TelegramClient } = require("telegram");
-const { StringSession } = require("telegram/sessions");
-const input = require("input"); 
+const { Api } = require("telegram/tl");
+const input = require("input");
+const { StoreSession } = require("telegram/sessions");
+require("dotenv").config();
 
+const storeSession = new StoreSession("my_session");
 const apiId = Number(process.env.API_ID);
 const apiHash = process.env.API_HASH;
-const stringSession = new StringSession(""); 
+const botToken = process.env.BOT_TOKEN;
+const userId = process.env.USER_ID;
+
+const targetChannels = ['rhymestg', 'rhymesmorgen', "testrisunya"]; 
 
 (async () => {
-  console.log("Loading interactive example...");
-  const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-  });
-  await client.start({
-    phoneNumber: async () => await input.text("Please enter your number: "),
-    password: async () => await input.text("Please enter your password: "),
-    phoneCode: async () =>
-      await input.text("Please enter the code you received: "),
-    onError: (err) => console.log(err),
-  });
-  console.log("You should now be connected.");
-  console.log(client.session.save()); // Save this string to avoid logging in again
-  await client.sendMessage("me", { message: "Hello!" });
+    const client = new TelegramClient(storeSession, apiId, apiHash, {
+        connectionRetries: 5,
+    });
+
+    await client.start({
+        phoneNumber: async () => await input.text("Введите номер телефона: "),
+        password: async () => await input.text("Введите пароль: "),
+        phoneCode: async () => await input.text("Введите код подтверждения: "),
+        onError: (err) => console.log(err),
+    });
+
+    console.log("Вы подключены к Telegram!");
+
+    const bot = new Bot(botToken);
+
+    bot.api.sendMessage(userId, "Бот запущен и слушает указанные каналы.");
+
+    // Присоединяемся к каналам
+    for (const channel of targetChannels) {
+        try {
+					const result = await client.invoke(
+						new Api.channels.JoinChannel({
+							channel: channel,
+						})
+					);
+					console.log(result && `Успешно подключен к @${channel}`);
+        } catch (error) {
+          console.warn(`Не удалось подключиться к каналу с ID ${channel}:`, error.message);
+        }
+    }
+
+    client.addEventHandler(async (event) => {
+			if (event.className !== "UpdateNewChannelMessage") return;
+			const id = (event.message.peerId.channelId).value.toString();
+
+			for (channel of targetChannels) {
+					const result = await client.invoke(
+						new Api.channels.GetFullChannel({
+							channel: channel,
+					}))
+					if (id == (result.chats[0].id).value.toString()) {
+						console.log(`Сообщение из группы ${result.chats[0].title}`)
+					};
+				};
+			} 
+    );
+
 })();
