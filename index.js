a735e97002991df2eff5124b93fc3173d3afcb06
risunya <risunyaaa@yandex.ru>
2025-02-19 09:23:07 +0300

    feat: swap to mtcute lib

--- index.js
+++ index.js
@@ -1,64 +1,20 @@
-const { Bot } = require("grammy");
-const { TelegramClient } = require("telegram");
-const { Api } = require("telegram/tl");
-const input = require("input");
-const { StoreSession } = require("telegram/sessions");
-require("dotenv").config();
+import { TelegramClient, html } from '@mtcute/node'
+import 'dotenv/config'
 
-const storeSession = new StoreSession("my_session");
 const apiId = Number(process.env.API_ID);
 const apiHash = process.env.API_HASH;
-const botToken = process.env.BOT_TOKEN;
-const userId = process.env.USER_ID;
 
-const targetChannels = ['rhymestg', 'rhymesmorgen', "testrisunya"]; 
+if (!apiId || !apiHash) {
+  throw new Error('API_ID и API_HASH должны быть установлены в переменных окружения');
+}
 
-(async () => {
-    const client = new TelegramClient(storeSession, apiId, apiHash, {
-        connectionRetries: 5,
-    });
+const tg = new TelegramClient({
+	apiId: apiId,
+  apiHash: apiHash,
+  storage: 'my-account' 
+})
 
-    await client.start({
-        phoneNumber: async () => await input.text("Введите номер телефона: "),
-        password: async () => await input.text("Введите пароль: "),
-        phoneCode: async () => await input.text("Введите код подтверждения: "),
-        onError: (err) => console.log(err),
-    });
+const self = await tg.start({})
+console.log(`Logged in as ${self.displayName}`)
 
-    console.log("Вы подключены к Telegram!");
-
-    const bot = new Bot(botToken);
-
-    bot.api.sendMessage(userId, "Бот запущен и слушает указанные каналы.");
-
-    // Присоединяемся к каналам
-    for (const channel of targetChannels) {
-        try {
-					const result = await client.invoke(
-						new Api.channels.JoinChannel({
-							channel: channel,
-						})
-					);
-					console.log(result && `Успешно подключен к @${channel}`);
-        } catch (error) {
-          console.warn(`Не удалось подключиться к каналу с ID ${channel}:`, error.message);
-        }
-    }
-
-    client.addEventHandler(async (event) => {
-			if (event.className !== "UpdateNewChannelMessage") return;
-			const id = (event.message.peerId.channelId).value.toString();
-
-			for (channel of targetChannels) {
-					const result = await client.invoke(
-						new Api.channels.GetFullChannel({
-							channel: channel,
-					}))
-					if (id == (result.chats[0].id).value.toString()) {
-						console.log(`Сообщение из группы ${result.chats[0].title}`)
-					};
-				};
-			} 
-    );
-
-})();
+await tg.sendText('self', html`Hello from <b>MTCute</b>!`)
